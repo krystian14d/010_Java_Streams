247387f853178d75b0f0dce7f0b6cce8e4b60421
Krystian <krystian14d@gmail.com>
2022-04-14 13:22:05 +0200

    sorting elements and objects

--- src/test/java/com/amigoscode/examples/Sorting.java
+++ src/test/java/com/amigoscode/examples/Sorting.java
@@ -15,21 +15,50 @@ public class Sorting {
     @Test
     public void sortingSteamOfElements() throws IOException {
         List<Person> people = MockData.getPeople();
+        List<String> sorted = people.stream()
+                .map(Person::getFirstName)
+                .sorted()
+                .collect(Collectors.toList());
+
+        sorted.forEach(System.out::println);
     }
 
     @Test
     public void sortingSteamOfElementsReverse() throws IOException {
         List<Person> people = MockData.getPeople();
+        List<String> sorted = people.stream()
+                .map(Person::getFirstName)
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+
+        sorted.forEach(System.out::println);
     }
 
     @Test
     public void sortingSteamOfObjets() throws IOException {
         List<Person> people = MockData.getPeople();
+
+        Comparator<Person> comparing = Comparator
+                .comparing(Person::getEmail)
+                .thenComparing(Person::getFirstName);
+
+        List<Person> sorted = people.stream()
+                .sorted(comparing)
+                .collect(Collectors.toList());
+
+        sorted.forEach(System.out::println);
     }
 
     @Test
     public void topTenMostExpensiveBlueCars() throws IOException {
         List<Car> cars = MockData.getCars();
+        List<Car> blueCars = cars.stream()
+                .filter(car -> car.getColor().equalsIgnoreCase("Blue"))
+                .sorted(Comparator.comparing(Car::getPrice).reversed())
+                .limit(10)
+                .collect(Collectors.toList());
+
+        blueCars.forEach(System.out::println);
     }
 
 }
